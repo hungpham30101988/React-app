a23284d1b302f4c895ab2d3416d769ba8cb47239
Hung Pham <>
2026-01-27 11:25:13 +0700

    testing from create modal

--- src/Api.js
+++ src/Api.js
@@ -12,6 +12,7 @@ const api = {
       mode: 'cors', // no-cors, cors, *same-origin
       redirect: 'follow', // manual, *follow, error
       referrer: 'no-referrer', // *client, no-referrer
+      testing: "testing123"
     })
     .then(response => response.json()) // parses response to JSON
   }
